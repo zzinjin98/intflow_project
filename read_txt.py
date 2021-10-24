2b4e08ca5f3060854cd5931f35dbcb2512aeb691
als90 <als90915@gmail.com>
2021-10-24 21:33:31 +0900

    2021.10.24 commit_2

--- read_txt.py
+++ read_txt.py
@@ -1,7 +1,19 @@
 import pandas as pd
+from Rotate_function import rotate, rotate_box_dot
 
-df = pd.read_csv("intflow_project-1\mounting_000_det.txt", sep=",", header=None)
+df = pd.read_csv("mounting_000_det.txt", sep=",", header=None)
 df.columns = ['frame','xc','yc','width','height','theta','no_x','no_y','ne_x','ne_y','ta_x','ta_y']
 df = df.drop(index=0, axis=0)
 
-print(df)
+rotate_list = []
+
+for i in range(len(df)):
+    data = list(df.loc[1,:])
+    result = rotate_box_dot(float(data[1]), float(data[2]), float(data[3]), float(data[4]), float(data[5]))
+    rotate_list.append(result)
+
+df_rotate = pd.DataFrame(rotate_list, columns=['rotated_x1','rotated_y1', 'rotated_x2','rotated_y2', 'rotated_x3','rotated_y3', 'rotated_x4','rotated_y4'])
+
+print(df_rotate)
+
+
