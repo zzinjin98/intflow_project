055c5cb05b0f60f1995f63a1bb099b5b7684bb5c
als90 <als90915@gmail.com>
2021-10-24 19:39:27 +0900

    2021.10.24 commit_1

--- read_txt.py
+++ read_txt.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("mounting_000_det.txt", sep=",", header=None)
+df = pd.read_csv("intflow_project-1\mounting_000_det.txt", sep=",", header=None)
 df.columns = ['frame','xc','yc','width','height','theta','no_x','no_y','ne_x','ne_y','ta_x','ta_y']
 df = df.drop(index=0, axis=0)
 
