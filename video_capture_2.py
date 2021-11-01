dd7e501905e8b66dbbb81ea89e34e732c9c3b2bb
als90 <als90915@gmail.com>
2021-11-01 14:40:01 +0900

    2021.11.01 commit_1

--- video_capture_2.py
+++ video_capture_2.py
@@ -5,6 +5,17 @@ from Rotate_function import rotate, rotate_box_dot
 
 def frame_extraction (text_url, video_url):
     cap = cv2.VideoCapture(video_url)
+    #재생할 파일의 넓이 얻기
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    #재생할 파일의 높이 얻기
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    #재생할 파일의 프레임 레이트 얻기
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+    # cv2.VideoWriter 객체 생성, 기존에 받아온 속성값 입력
+    out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
 
     df = pd.read_csv(text_url, sep=",", header=None)
     frame = 0
@@ -22,19 +33,23 @@ def frame_extraction (text_url, video_url):
                     points = rotate_box_dot(i[0], i[1], i[2], i[3], i[4])
                     img = cv2.polylines(img,[points],True,(color,color,color),thickness=3)
                     img = cv2.putText(img, str(int(i[11])), (int(i[0]), int(i[1])), cv2.FONT_HERSHEY_PLAIN, 2, (color,color,color), thickness=2)
-
+                    
+                out.write(img)
                 cv2.imshow(video_url, img)
                 cv2.waitKey(25)
 
                 frame+=1
             else:
                 break
-
+                   
     else:
         print("can't open video")
-
+        #재생할 파일의 넓이와 높이
+    
+    out.release()
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     frame_extraction("..\mounting_000\mounting_000_trk.txt", "..\mounting_000\mounting_000.mp4")
